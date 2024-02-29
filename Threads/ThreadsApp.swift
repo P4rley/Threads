80ac405f8103b462d32d7942775afe4debe833fe
P4rley <adwihersa@gmail.com>
2024-02-29 10:15:08 +0700

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Adwitya Hersa on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
